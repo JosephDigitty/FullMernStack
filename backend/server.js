16bb367000f7dc571116eb317ad532c416727c1d
JosephDigitty <olujosephdreal@yahoo.com>
2024-08-23 19:02:47 +0100

    addedsomething new

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 
-const port = 5000
+const port = process.env.PORT || 5000
 const app = express ()
+
+
+app.get("/api/goals", require("./routes/goalroutes"))
 
 app.listen(port, () => console.log(`listening on port: ${port}`));
